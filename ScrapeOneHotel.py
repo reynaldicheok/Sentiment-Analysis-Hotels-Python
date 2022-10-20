5ce61f5d45ae653ff4a971d634a32081a9b507b0
kazoohaa <114292780+kazoohaa@users.noreply.github.com>
2022-10-20 13:52:59 +0800

    Update ScrapeOneHotel.py

--- ScrapeOneHotel.py
+++ ScrapeOneHotel.py
@@ -1,166 +1,146 @@
-import time
-import csv
-import requests
-import scrapy
-from bs4 import BeautifulSoup
 import pandas as pd
-from scrapy.crawler import CrawlerProcess
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
-from geopy.extra.rate_limiter import RateLimiter
+import numpy as np
 
-input_url = 'https://www.booking.com/reviews/sg/hotel/four-season-singapore.en-gb.html?aid=356980&label=gog235jc-1FEgdyZXZpZXdzKIICOOgHSDNYA2jJAYgBAZgBCbgBF8gBDNgBAegBAfgBDYgCAagCA7gCgrj9mAbAAgHSAiQ1NjY2NDdjNy03NjEzLTRiNjEtYjQ1OC04MDk1Y2M2MzhlYjLYAgbgAgE&sid=0592c1baed62f1328376ae7ea3a086ed&customer_type=total&hp_nav=0&old_page=0&order=featuredreviews&page=1&r_lang=en&rows=75&'
-
-
-def review_check_pos(positive_review):
-    pos_review = [t.get_text(strip=True) for t in positive_review.find_all('p', attrs={
-        'class': 'review_pos'})]
-    if pos_review == []:
-        return False
-    elif pos_review != []:
-        return True
-
-def review_check_neg(negative_review):
-    neg_review = [t.get_text(strip=True) for t in negative_review.find_all('p', attrs={
-        'class': 'review_neg'})]
-    if neg_review == []:
-        return False
-    elif neg_review != []:
-        return True
-
-def scrapeone(x):
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36'
-    }
-    start_url = x
-
-    s = Service(
-        r'D:\School\INF1002\Week2\chromedriver.exe')  # Local file location for chromedriver.exe to use selenium to use the webbrowser
-
-    driver = webdriver.Chrome(service=s)  # To get the chrome service started
-    driver.get(start_url)  # To go to the url
-    time.sleep(1)
-    # driver.find_element(By.CLASS_NAME,"hp-review-score-cta-container-remote").click()
-    # time.sleep(2)
-    page_source = driver.page_source
-    with open('pagetestzz.html', 'w+', encoding="utf-8") as f:
-        f.write(driver.page_source)  # Saves and writes the page source or html code locally
-
-    writeheader = True
-    while True:
-        try:
-            newurl = driver.current_url
-            print(newurl)
-            driver.get(newurl)
-            page_source = driver.page_source
-            soup = BeautifulSoup(page_source, 'lxml')  # Parses the data/html code
-
-            hotelname = [t.get_text(strip=True) for t in
-                         soup.find_all('a', attrs={'class': 'standalone_header_hotel_link'})]
-            hoteladdress = [t.get_text(strip=True) for t in soup.find_all('p', attrs={'class': 'hotel_address'})]
-            review_score = [t.get_text(strip=True) for t in
-                            soup.find_all('span', attrs={'class': 'review-score-badge'})]
-            review_score = review_score[1::]
-            # test_texxt=soup.find_all("span", itemprop="reviewBody") #Retrieves all text within the reviewBody so mixes both positive and negative
-
-            review_fix = [t.get_text for t in soup.find_all("div", attrs={
-                "class": "review_item_review_content"})]  # Retrieves all text within the reviewBody so mixes both positive and negative
-            review_pos = []
-            review_neg = []
-            for reviews in review_fix:
-
-                review_fix_test = BeautifulSoup(str(reviews), 'lxml')
-                pos_review = [t.get_text(strip=True) for t in review_fix_test.find_all('p', attrs={
-                    'class': 'review_pos'})]
-                neg_review = [t.get_text(strip=True) for t in review_fix_test.find_all('p', attrs={
-                    'class': 'review_neg'})]
-                if review_check_pos(review_fix_test) == True:
-                    review_pos.append(pos_review)
-                if review_check_pos(review_fix_test) == False:
-                    review_pos.append([""])
-                if review_check_neg(review_fix_test) == True:
-                    review_neg.append(neg_review)
-                if review_check_neg(review_fix_test) == False:
-                    review_neg.append([""])
-            review_negz = [item for sublist in review_neg for item in sublist]
-            review_negz = [item.replace('\n', '') for item in review_negz]
-
-            review_posz = [item for sublist in review_pos for item in sublist]
-            review_posz = [item.replace('\n', '') for item in review_posz]
-
-            'Grab just postal code'
-            postalcodecountry = hoteladdress[0].split(' ')
-            postalcode = ''
-            for x in postalcodecountry:  # Check array of split text for 6 digit postal code
-                count = sum(map(str.isdigit, x))
-                if count >= 5:
-                    postalcode = x
-            postalcodefinal = ''.join(c for c in str(postalcode) if c.isdigit())
-
-            from geopy.geocoders import Nominatim
-
-            geolocator = Nominatim(user_agent="geoapiExercises")
-            location = geolocator.geocode(postalcodefinal+ ' SG', exactly_one=True, timeout=60)
-            getLoc = location.raw
-
-            updatedscore = []
-            # Append Review Score together with Review
-            review_score = [float(x) for x in review_score]
-            review_score = [int(x) for x in review_score]
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+from nltk.tokenize import RegexpTokenizer
+tokenizer = RegexpTokenizer(r'\w+')
+import matplotlib.pyplot as plt
+import seaborn as sns
+color = sns.color_palette()
+import string
 
 
-            for x in review_score:
-                if int(x) < 5:
-                    updatedscore.append(1)
-                if int(x) == 6:
-                    updatedscore.append(2)
-                if int(x) == 7:
-                    updatedscore.append(3)
-                if int(x) == 8:
-                    updatedscore.append(4)
-                if int(x) == 9:
-                    updatedscore.append(5)
-                if int(x) == 10:
-                    updatedscore.append(5)
+input_url = 'https://www.booking.com/reviews/sg/hotel/citadines-rochor.en-gb.html?aid=356980&sid=4581fecdf88e3c532e910a9a05e6eb81&label=gog235jc-1FEgdyZXZpZXdzKIICOOgHSDNYA2jJAYgBAZgBCbgBF8gBDNgBAegBAfgBDYgCAagCA7gCgrj9mAbAAgHSAiQ1NjY2NDdjNy03NjEzLTRiNjEtYjQ1OC04MDk1Y2M2MzhlYjLYAgbgAgE'
 
+# initializing empty list where we are going to have words counted
+stopwords = set(stopwords.words('english'))
+stopwords.update(["br","href","hotel","room","rooms","stay","stayed","would","could","really","get","also","us","one","time","great",
+                      "good","bad","nice","like","best","review","reviews","negative","positive","little","well","check","super",
+                      "quite","india","singapore","malaysia","china","japan","korea","philippines","door","day","night","everything",
+                      "nothing","entrance","need","first","last","close","even","in","pillow","pillows","still","wet","value","late",
+                      "guest","guests","always","morning","sleep","places","feel","feels","people","floor","front","worth","gone",
+                      "walking","lovely","love","made","make","plus","overall","air","free","pressure","it","access","go","wonderful",
+                      "probably","2","near","staying","within","better","request","around","size","long","way","activity","too","got"
+                      "activities","two","loved","especially","much","property","back","desk","in","1","bit","ice","tap","told","side",
+                      "water","take","cream","new","excellent","different","though","lobby","took","well","walk","available","next","3",
+                      "wait","lack","perfect","book","booked","booking","early","fun","ask","asked","enough","amazing","rail","rails",
+                      "easy","hard","difficult","extremely","extreme","4","smell","5","6","7","8","9","10","money","every","food","space",
+                      "distance","many","hear","said","pay","days","ok","okay","price","luggage","pretty","fast","all","due","noise","dirty",
+                      "filthy","greasy","sooty","moldy","grimy","grubby","soiled","unwashed","stain","stained","spotted","cloudy","muddy",
+                      "dusty","paper","street","another","open","corridor","outside","nice","poor","use","rude","lavender","want","theres",
+                      "see","friendly","helpful","disgusting","terrible","areas","liked","needed","turn","keep","short","tall","come","came",
+                      "went","change","changes","found","home","working","call","arrived","arrive","station","stations","find","hot","freeze"
+                      "centrally","located","things","work","instead","again","allow","allowed","requested","right","left","freezing","cold"
+                      "standard","enjoy","machine","proper","definitely","find","needs","extra","building","small","noisy","nil"])
 
-            combined = zip(review_posz, review_negz, updatedscore)
+def top15words():
+    csvfile = scrapeone(input_url)
+    df = pd.read_csv(csvfile, encoding = "ISO-8859-1")
 
-            combined2 = []
-            for i in combined:
-                addin = postalcodefinal, getLoc['lat'], getLoc['lon'], i[0], i[1], i[2]
-                combined2.append(addin)
-
-            'print output to csv'
-
-            with open(hotelname[0] + ".csv", "a", encoding="utf-8", newline='') as csvFile:
-                fieldnames = ['hotelname', 'postalcode', 'latitude', 'longitude', 'review_pos', 'review_neg', 'review_text', 'review-score']
-                writer = csv.DictWriter(csvFile, fieldnames=fieldnames)
-                if writeheader == True:
-                    writer.writeheader()
-                    writeheader = False
-                for item in combined2:
-                    writer.writerow(
-                        {'hotelname': hotelname[0], 'postalcode': item[0], 'latitude': item[1], 'longitude': item[2],
-                         'review_pos': item[3], 'review_neg': item[4], 'review_text': str(item[3]) +' '+ str(item[4]),'review-score': item[5]})
-
-            with open('SingaporeHotel.csv', "a", newline='', encoding="utf-8") as csvFile:
-                fieldnames = ['hotelname', 'postalcode', 'latitude', 'longitude', 'review_pos', 'review_neg','review_text', 'review-score']
-                writer = csv.DictWriter(csvFile, fieldnames=fieldnames)
-                for item in combined2:
-                    writer.writerow(
-                        {'hotelname': hotelname[0], 'postalcode': item[0], 'latitude': item[1], 'longitude': item[2],
-                         'review_pos': item[3], 'review_neg': item[4], 'review_text': str(item[3]) + ' ' + str(item[4]),
-                         'review-score': item[5]})
-            csvFile.close()
-            driver.find_element("xpath", "//*[contains(@id, 'review_next_page_link')]").click()
-        except:
-            print("fail or end of pages")
-            break
-    hoteloutputcsv = hotelname[0] + ".csv"
-    return hoteloutputcsv
-    driver.quit()
-
-
-test = scrapeone(input_url)
+    #synonym check
+    _review_ = []
+    # getting review strings and appending it to the _review_ list
+    for review in df.review_text:
+        _review_.append(review)
+    
+    # setting a function that will split those reviews strings into separate words
+    def split_name(review):
+        spl = str(review).split()
+        return spl
+    
+    
+    _review_count_ = []
+    
+    # getting name string from our list and using split function, later appending to list above
+    # Punctuation: !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~
+    punctuation = [",",";",":","?",".","\'","\\","-","(",")","+","#","@","<",">","_","{","}","[","]","/","*","%"]
+    
+    for x in _review_:
+        for word in split_name(x):
+            if word in punctuation:
+                continue
+            else:
+                word = word.strip() #removes whitespace
+                word = word.lower()
+                if word not in stopwords:
+                    word = word.translate(str.maketrans('', '', string.punctuation))
+                    _review_count_.append(word)
+    
+    # we are going to use counter
+    from collections import Counter
+    
+    comfortable_synonyms = ["comfy","cosy","warm","pleasant","enjoyable","agreeable","congenial","plush","secure","safe","homely","snuggly","enjoyed"]
+    service_synonyms = ["staff","staffs","servicing","assistance","help","provided","good","great"]
+    dorm_synonyms = ["dorms"]
+    apartment_synonyms = ["apartments"]
+    toilet_synonyms = ["toilets","bathroom","bathrooms","lavatory","lavatories","urinal"]
+    pool_synonyms = ["pools","swim","swimming"]
+    kid_synonyms = ["kid","kids","children","child","youngster","baby","toddler","infant","minor","adolescent","teenager","youth"]
+    restaurant_synonyms = ["restaurants","eatery"]
+    aircon_synonyms = ["ac","aircon","air-con","aircondition","air-condition","airconditioner","air-conditioner"]
+    location_synonyms = ["spot","place","area","central","centrally","nearby","india"]
+    cleanliness_synonyms = ["clean","neat","sanitize","sanitise","spotless","sanittion","disinfect"]
+    spacious_synonyms = ["wide","big","roomy","vast","large","palatial","huge"]
+    facilities_synonyms = ["amenity","facility","amenities","convenience","convenient"]
+    housekeeping_synonyms = ["cleaning","equipped","cleaned","towel","towels","toiletries"]
+    family_synonyms = ["family","families","household"]
+    #_synonyms = []
+    for i in range(len(_review_count_)):
+        if _review_count_[i] in comfortable_synonyms:
+            _review_count_[i] = "comfortable"
+        elif _review_count_[i] in service_synonyms:
+            _review_count_[i] = "service"
+        elif _review_count_[i] in dorm_synonyms:
+            _review_count_[i] = "dorm"
+        elif _review_count_[i] in apartment_synonyms:
+            _review_count_[i] = "apartment"
+        elif _review_count_[i] in toilet_synonyms:
+            _review_count_[i] = "toilet"
+        elif _review_count_[i] in pool_synonyms:
+            _review_count_[i] = "pool"
+        elif _review_count_[i] in kid_synonyms:
+            _review_count_[i] = "kid-friendly"
+        elif _review_count_[i] in restaurant_synonyms:
+            _review_count_[i] = "restaurant"
+        elif _review_count_[i] in aircon_synonyms:
+            _review_count_[i] = "air-conditioned"
+        elif _review_count_[i] in location_synonyms:
+            _review_count_[i] = "location"
+        elif _review_count_[i] in cleanliness_synonyms:
+            _review_count_[i] = "cleanliness"
+        elif _review_count_[i] in spacious_synonyms:
+            _review_count_[i] = "spacious"
+        elif _review_count_[i] in facilities_synonyms:
+            _review_count_[i] = "facilities"
+        elif _review_count_[i] in housekeeping_synonyms:
+            _review_count_[i] = "housekeeping"
+        elif _review_count_[i] in family_synonyms:
+            _review_count_[i] = "family-friendly"
+        else:
+            continue
+    
+    _top_15_w = Counter(_review_count_).most_common()
+    # Sort number of words viewed
+    _top_15_w = _top_15_w[0:15]
+    
+    sub_w=pd.DataFrame(_top_15_w)
+    sub_w.rename(columns={0:'Words', 1:'Count'}, inplace=True)
+    
+    # Defining the plot size
+    plt.figure(figsize=(10,6))
+    # Defining the values for x-axis & y-axis and from which dataframe the values are to be picked
+    viz_5=sns.barplot(x='Count', y='Words', data=sub_w)
+    viz_5.set_title('Counts of the top 30 used words for reviews')
+    viz_5.set_ylabel('Words')
+    viz_5.set_xlabel('Count of words')
+    viz_5.set_xticklabels(viz_5.get_xticklabels(), rotation=80)
+    for i in viz_5.patches:
+        viz_5.text(i.get_width()+0.2, i.get_y()+0.5,
+                 str(round((i.get_width()))),
+                 fontsize = 10, color ='black')
+    plt.savefig('top15words.png')
+    plt.show()
+    
+top15words()
