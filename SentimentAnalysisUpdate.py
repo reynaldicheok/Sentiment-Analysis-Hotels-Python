b67686cb2a0ff9f23735e58c820820573b3f57fb
reynaldicheok <44896387+reynaldicheok@users.noreply.github.com>
2022-10-20 12:00:35 +0800

    Update SentimentAnalysisUpdate.py
    Update for scrape 2 page and top 10 reviews

--- SentimentAnalysisUpdate.py
+++ SentimentAnalysisUpdate.py
@@ -1,20 +1,20 @@
 import csv
-
+import time
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-
+from selenium import webdriver
 color = sns.color_palette()
 from wordcloud import WordCloud
 import numpy as np
 import plotly.offline as py
-
+from bs4 import BeautifulSoup
 import plotly.graph_objs as go
 import plotly.tools as tls
 import plotly.express as px
 
 import nltk
-
+from selenium.webdriver.chrome.service import Service
 nltk.download('stopwords')
 from nltk.corpus import stopwords
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
@@ -22,6 +22,7 @@ import nltk
 
 nltk.download('vader_lexicon')
 vader = SentimentIntensityAnalyzer()
+input_url = 'https://www.booking.com/reviews/sg/hotel/the-barracks-by-far-east-hospitality.en-gb.html?aid=356980&label=gog235jc-1FEgdyZXZpZXdzKIICOOgHSDNYA2jJAYgBAZgBCbgBF8gBDNgBAegBAfgBDYgCAagCA7gCgrj9mAbAAgHSAiQ1NjY2NDdjNy03NjEzLTRiNjEtYjQ1OC04MDk1Y2M2MzhlYjLYAgbgAgE&sid=248efadb06977d69b94338011302293d'
 
 
 # Use this function to read the file, input file path as input
@@ -30,6 +31,22 @@ def read_csv(filename):
     return csv_file_df
 
 
+def review_check_pos(positive_review):
+    pos_review = [t.get_text(strip=True) for t in positive_review.find_all('p', attrs={
+        'class': 'review_pos'})]
+    if pos_review == []:
+        return False
+    elif pos_review != []:
+        return True
+
+
+def review_check_neg(negative_review):
+    neg_review = [t.get_text(strip=True) for t in negative_review.find_all('p', attrs={
+        'class': 'review_neg'})]
+    if neg_review == []:
+        return False
+    elif neg_review != []:
+        return True
 # Call this function and map it to a variable
 
 # Use this function to generate a graph for data visualization with reference to the function read_csv
@@ -121,6 +138,25 @@ def lowest_review(csvfile): #most negative review
     output=finaltest[0][0::]
     return output
 
+def top_ten_pos(csvfile):
+    reader = csvfile
+
+    mostpositive = reader.nlargest(10,['review-score','compound'])
+
+    finaltest = mostpositive.values.tolist()
+    output = finaltest
+    return output
+
+def top_ten_neg(csvfile):
+    reader = csvfile
+
+    mostpositive = reader.nsmallest(10,['review-score','compound'])
+
+    finaltest = mostpositive.values.tolist()
+    output = finaltest
+    return output
+
+
 def highest_review(csvfile): #Most positive review
     reader=csvfile
     mostpositive=reader['compound'].max()
@@ -190,6 +226,141 @@ def rating_further(csvfile):#DAta display with the neutral positive and neutral 
               bbox_to_anchor=(1, 1))
     plt.show()
 
+def scrapeone(x):
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36'
+    }
+    start_url = x
+
+    s = Service(
+        r'C:\Users\Reynaldi\Desktop\chromedriver.exe')  # Local file location for chromedriver.exe to use selenium to use the webbrowser
+
+    driver = webdriver.Chrome(service=s)  # To get the chrome service started
+    driver.get(start_url)  # To go to the url
+    time.sleep(1)
+    # driver.find_element(By.CLASS_NAME,"hp-review-score-cta-container-remote").click()
+    # time.sleep(2)
+    page_source = driver.page_source
+    with open('pagetestzz.html', 'w+', encoding="utf-8") as f:
+        f.write(driver.page_source)  # Saves and writes the page source or html code locally
+
+    writeheader = True
+    for x in range(2):
+
+        newurl = driver.current_url
+        print(newurl)
+        driver.get(newurl)
+        page_source = driver.page_source
+        soup = BeautifulSoup(page_source, 'lxml')  # Parses the data/html code
+
+        hotelname = [t.get_text(strip=True) for t in
+                     soup.find_all('a', attrs={'class': 'standalone_header_hotel_link'})]
+        hoteladdress = [t.get_text(strip=True) for t in soup.find_all('p', attrs={'class': 'hotel_address'})]
+        review_score = [t.get_text(strip=True) for t in soup.find_all('span', attrs={'class': 'review-score-badge'})]
+        review_score = review_score[1::]
+        # test_texxt=soup.find_all("span", itemprop="reviewBody") #Retrieves all text within the reviewBody so mixes both positive and negative
+
+        review_fix = [t.get_text for t in soup.find_all("div", attrs={
+            "class": "review_item_review_content"})]  # Retrieves all text within the reviewBody so mixes both positive and negative
+        review_pos = []
+        review_neg = []
+        for reviews in review_fix:
+
+            review_fix_test = BeautifulSoup(str(reviews), 'lxml')
+            pos_review = [t.get_text(strip=True) for t in review_fix_test.find_all('p', attrs={
+                'class': 'review_pos'})]
+            neg_review = [t.get_text(strip=True) for t in review_fix_test.find_all('p', attrs={
+                'class': 'review_neg'})]
+            if review_check_pos(review_fix_test) == True:
+                review_pos.append(pos_review)
+            if review_check_pos(review_fix_test) == False:
+                review_pos.append([""])
+            if review_check_neg(review_fix_test) == True:
+                review_neg.append(neg_review)
+            if review_check_neg(review_fix_test) == False:
+                review_neg.append([""])
+        review_negz = [item for sublist in review_neg for item in sublist]
+        review_negz = [item.replace('\n', '') for item in review_negz]
+
+        review_posz = [item for sublist in review_pos for item in sublist]
+        review_posz = [item.replace('\n', '') for item in review_posz]
+
+        'Grab just postal code'
+        postalcodecountry = hoteladdress[0].split(' ')
+        postalcode = ''
+        for x in postalcodecountry:  # Check array of split text for 6 digit postal code
+            count = sum(map(str.isdigit, x))
+            if count >= 5:
+                postalcode = x
+        postalcodefinal = ''.join(c for c in str(postalcode) if c.isdigit())
+
+        from geopy.geocoders import Nominatim
+
+        geolocator = Nominatim(user_agent="geoapiExercises")
+        location = geolocator.geocode('Singapore ' + postalcodefinal)
+        getLoc = location.raw
+
+        updatedscore = []
+
+        # Append Review Score together with Review
+        review_score = [float(x) for x in review_score]
+        review_score = [int(x) for x in review_score]
+
+        for x in review_score:
+            if int(x) < 5:
+                updatedscore.append(1)
+            if int(x) == 6:
+                updatedscore.append(2)
+            if int(x) == 7:
+                updatedscore.append(3)
+            if int(x) == 8:
+                updatedscore.append(4)
+            if int(x) == 9:
+                updatedscore.append(5)
+            if int(x) == 10:
+                updatedscore.append(5)
+        scoreandreview = []
+        scorecount = 1
+
+        combined = zip(review_posz, review_negz, updatedscore)
+
+        combined2 = []
+        for i in combined:
+            addin = postalcodefinal, getLoc['lat'], getLoc['lon'], i[0], i[1], i[2]
+            combined2.append(addin)
+
+        'print output to csv'
+
+        with open(hotelname[0] + ".csv", "a", encoding="utf-8", newline='') as csvFile:
+            fieldnames = ['id', 'postalcode', 'latitude', 'longitude', 'review_pos', 'review_neg', 'review-score']
+            writer = csv.DictWriter(csvFile, fieldnames=fieldnames)
+            if writeheader == True:
+                writer.writeheader()
+                writeheader = False
+            for item in combined2:
+                writer.writerow({'id': hotelname[0], 'postalcode': item[0], 'latitude': item[1], 'longitude': item[2],
+                                 'review_pos': item[3], 'review_neg': item[4], 'review-score': item[5]})
+
+        with open('datafiniti_hotel_reviews (2) - Copy.csv', "a", newline='', encoding="utf-8") as csvFile:
+            fieldnames = ['id', 'dateadded', 'dateupdated', 'address', 'categories', 'primarycategories', 'city',
+                          'country',
+                          'keys', 'latitude', 'longitude', 'name', 'postalcode', 'province', 'reviews_date',
+                          'reviews_dateseen', 'reviews_rating', 'reviews_sourceurls', 'reviews_text']
+            writer = csv.DictWriter(csvFile, fieldnames=fieldnames)
+            for item in combined2:
+                writer.writerow(
+                    {'id': '', 'dateadded': '', 'dateupdated': '', 'address': '', 'categories': '',
+                     'primarycategories': '',
+                     'city': '', 'country': '', 'keys': '', 'latitude': item[1], 'longitude': item[2],
+                     'name': hotelname[0], 'postalcode': item[0], 'province': '', 'reviews_date': '',
+                     'reviews_dateseen': '', 'reviews_rating': item[5], 'reviews_sourceurls': '',
+                     'reviews_text': item[3] + item[4]})
+        csvFile.close()
+        driver.find_element("xpath", "//*[contains(@id, 'review_next_page_link')]").click()
+
+    driver.quit()
+
+
 
 if __name__ == '__main__':
     # Sample on how to use the whole program
@@ -203,6 +374,8 @@ if __name__ == '__main__':
     new_dataframez = breakdown_dataframe(dataframez)
     rating(new_dataframez)
     rating_further(new_dataframez)
+    print(top_ten_pos(new_dataframez))
+    scrapeone(input_url)
     #print(new_dataframez.head())
 
     #word_cloud_gen = wordcloud_gen(new_dataframez)
